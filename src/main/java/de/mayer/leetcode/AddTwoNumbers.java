b7f8087480fdc9d2e5da862f3457e090462f11c6
xome <johannes@einmayer.de>
2024-02-05 13:37:38 +0100

    Initial commit

---
+++ src/main/java/de/mayer/leetcode/AddTwoNumbers.java
@@ -0,0 +1,18 @@
+package de.mayer.leetcode;
+
+public class AddTwoNumbers {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
